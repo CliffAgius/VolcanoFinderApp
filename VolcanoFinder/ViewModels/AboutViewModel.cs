afff22518f24d615b7274787b15313c94bde04ab
Clifford Agius <cliffordagius@ca-software.co.uk>
2021-04-12 22:57:38 +0100

    Added the Logout on the About Page...

--- VolcanoFinder/ViewModels/AboutViewModel.cs
+++ VolcanoFinder/ViewModels/AboutViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Windows.Input;
+using VolcanoFinder.Services;
+using Xamarin.CommunityToolkit.ObjectModel;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
@@ -7,10 +10,20 @@ namespace VolcanoFinder.ViewModels
 {
     public class AboutViewModel : BaseViewModel
     {
+
+        public IAsyncCommand LogOutCommand { get; set; }
+
         public AboutViewModel()
         {
+            LogOutCommand = new AsyncCommand(ActionLogOut);
             Title = "About";
             OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+        }
+
+        private async Task ActionLogOut()
+        {
+            await AuthService.SignOut();
+            await Shell.Current.GoToAsync($"//LoginPage");
         }
 
         public ICommand OpenWebCommand { get; }
